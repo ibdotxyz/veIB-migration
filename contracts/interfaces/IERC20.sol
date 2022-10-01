4f2a6a2503156a874fbf256b34c840a4dcc1f732
regan <regan@ib.xyz>
2022-10-01 14:52:03 +0800

    mint to return bool, consistent with IBtoken on OP

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-    function mint(address to, uint256 amount) external;
+    function mint(address to, uint256 amount) external returns (bool);
 
     function approve(address spender, uint256 value) external returns (bool);
 }
